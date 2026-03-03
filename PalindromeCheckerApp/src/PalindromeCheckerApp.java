44b66e7e83082fd3a30e0b0196bcd686a7cb48a7
Bhavy <bhavyb1234@gmail.com>
2026-03-03 15:36:37 +0530

    UC7

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,41 +1,27 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
-public class UseCase6PalindromeCheckerApp {
-
+public class UseCase7PalindromeCheckerApp {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        Deque<Character> deque = new ArrayDeque<>();
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
-
-        Stack<Character> stack = new Stack<>();
-        Queue<Character> queue = new LinkedList<>();
-
-        for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            stack.push(ch);
-            queue.add(ch);
+        for (char c : input.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                deque.addLast(Character.toLowerCase(c));
+            }
         }
 
         boolean isPalindrome = true;
 
-        while (!stack.isEmpty()) {
-            if (!stack.pop().equals(queue.remove())) {
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        if (isPalindrome) {
-            System.out.println(input + " is a Palindrome.");
-        } else {
-            System.out.println(input + " is NOT a Palindrome.");
-        }
-
-        scanner.close();
+        System.out.println(isPalindrome ? "Palindrome" : "Not Palindrome");
+        sc.close();
     }
 }
