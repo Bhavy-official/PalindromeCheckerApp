a20ca6dfd0b25aa3500b9cf4b2847631ba9a73bf
Bhavy <bhavyb1234@gmail.com>
2026-03-03 16:24:31 +0530

    UC13

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -5,31 +5,30 @@ public class PalindromeCheckerApp {
 
         PalindromeStrategy strategy = new StackStrategy();
 
+        long startTime = System.nanoTime();
         boolean isPalindrome = strategy.check(input);
+        long endTime = System.nanoTime();
+
+        long duration = endTime - startTime;
 
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Execution Time : " + duration + " ns");
     }
 }
-
 
 interface PalindromeStrategy {
     boolean check(String input);
 }
 
-
 class StackStrategy implements PalindromeStrategy {
 
-    @Override
     public boolean check(String input) {
-
         java.util.Stack<Character> stack = new java.util.Stack<>();
-
 
         for (char c : input.toCharArray()) {
             stack.push(c);
         }
-
 
         for (char c : input.toCharArray()) {
             if (c != stack.pop()) {
